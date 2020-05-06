95090c0c58ec7a2d716116734d0dbc60c2ebaad4
Mahmoud Zod <mzod@overip.io>
2020-05-06 16:16:59 +1000

    updated TO DO task

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -15,6 +15,7 @@ FORTUNES = [
 
 def generate_fortune() -> str:
     """Use mystical forces (a random selection) to get a user's fortune."""
+    #print(random.choice(FORTUNES))
     return random.choice(FORTUNES)
 
 
@@ -34,8 +35,14 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
     # cookie's message.
+    fortune = generate_fortune()
+    lucky_numbers = generate_lucky_numbers(how_many_lucky_numbers)
 
-    raise NotImplementedError()
+    return "{fortune}\nLucky Numbers: {lucky_numbers}".format(
+        fortune = fortune,
+        lucky_numbers = lucky_numbers,
+    )
+    #raise NotImplementedError()
 
 
 def main():
